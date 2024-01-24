066580986ab9db56cc98dee37325d6dd9461abd6
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-24 00:56:57 -0300

    ContainerHome Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 //import axios from 'axios';
+import { ContainerHome } from '../components';
 
 export const Home = () => {
     return (
         <ContainerHome>Test</ContainerHome>
     );
 };
-
-const ContainerHome = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    width: 100%;
-    height: 100vh;
-    margin: 0;
-`;
